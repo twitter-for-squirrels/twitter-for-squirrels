ecc4e153799a19134bf8a21eea7f890ba4497dcf
Lorenzo Pisani <zeelot3k@gmail.com>
2013-05-25 12:04:15 -0700

    add route for posts api endpoint

--- application/routes/default.php
+++ application/routes/default.php
@@ -4,8 +4,22 @@
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
-Route::set('default', '(<controller>(/<action>(/<id>)))')
+Route::set('home', '')
 	->defaults(array(
 		'controller' => 'main',
 		'action'     => 'index',
 	));
+
+Route::set('api.posts', 'api/posts(/<id>)')
+	->defaults(array(
+		'directory' => 'API',
+		'controller' => 'Posts'
+	))
+	->filter(function($route, $params, $request) {
+		// Checks whether this request is for a collection or entity.
+		$action_on = isset($params['id']) ? 'entity' : 'collection';
+		// Prefix the controller action with the request method.
+		$params['action'] = $request->method().'_'.$action_on;
+
+		return $params;
+	});
