856f704b724d1f3915097b440bf8f69730f1954a
Lorenzo Pisani <zeelot3k@gmail.com>
2013-05-25 11:48:35 -0700

    enable db and migrations modules

--- application/config/modules.php
+++ application/config/modules.php
@@ -3,7 +3,7 @@
 return array(
 	'assets'             => MODPATH.'assets',
 //	'auth'               => MODPATH.'auth',
-//	'database'           => MODPATH.'database',
+	'database'           => MODPATH.'database',
 	'git'                => MODPATH.'git',
 //	'image'              => MODPATH.'image',
 	'kostache'           => MODPATH.'kostache',
@@ -11,6 +11,7 @@ return array(
 	'minion'             => MODPATH.'minion',
 //	'minion-repo'        => MODPATH.'minion-tasks-repo',
 	'minion-media'       => MODPATH.'minion-tasks-media',
+	'minion-migrations'  => MODPATH.'minion-tasks-migrations',
 //	'orm'                => MODPATH.'orm',
 //	'userguide'          => MODPATH.'userguide',
 //	'yform'              => MODPATH.'yform',
